14cc324f8a4b2a89af0ce55ea3a2a0a1c21bf262
Ahmad Azizi <azizi153.97@gmail.com>
2024-04-25 21:26:41 +0700

    materi 1

---
+++ reactjs-task/src/App.jsx
@@ -0,0 +1,12 @@
+import Task1 from "./components/Task1"
+import img from "./assets/Pemandangan.jpg"
+
+function App() {
+  return (
+ <div className="App">
+  <Task1 img={img} title="Pemandangan"/>
+ </div>
+  )
+}
+
+export default App
---
+++ reactjs-task/src/components/Task1.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Task1(props){
+    return(
+        <div className="h-screen flex justify-center items-center">
+             <div className="w-64 h-64 border border-emerald-400 rounded-lg flex flex-col justify-between items-center p-4 text-emerald-800">
+                <img src={props.img} alt="gambar" />
+                <p>{props.title}</p>    
+            </div>  
+        </div>
+    )
+}
